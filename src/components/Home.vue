017a14e60669a176546ecca21bf70fa428b72ab8
fmabid <fmaabid@gmail.com>
2022-01-20 00:18:18 +0600

    Adding router and dropzone 3rd party pkg

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,16 @@
+<template>
+  <div class="text-center space-x-4 py-4">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+
+  <div class="bg-yellow-100 w-full px-12">j</div>
+</template>
+
 <script setup lang="ts">
 import { ref } from "vue";
 
-// defineProps<{ msg: string }>();
-
 const count = ref(0);
 </script>
-
-<template>
-  <router-link to="/">Home</router-link>
-  <router-link to="/about">About</router-link>
-</template>
 
 <style scoped></style>
