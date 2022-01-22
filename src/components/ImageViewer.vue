29ebc011113f64d410b083dfd5b9d121cc5d9d50
fmabid <fmaabid@gmail.com>
2022-01-22 15:46:45 +0600

    show preview after uploading

--- src/components/ImageViewer.vue
+++ src/components/ImageViewer.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>image</div>
+  <div>
+    <div v-if="imgPath">
+      <img :src="imgPath" />
+    </div>
+
+    <Uploader @on-uploaded="updateImagePath" />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+import Uploader from "@/components/Uploader.vue";
+
+const imgPath = ref("");
+
+function updateImagePath(file) {
+  imgPath.value = URL.createObjectURL(file);
+
+  var image = new Image();
+  var reader = new FileReader();
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,11 +2,13 @@
   <div class="text-center space-x-4 py-4">
     <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
-
-    <ImageViewer />
   </div>
 
-  <div class="bg-yellow-100 w-full px-12">j</div>
+  <div class="bg-yellow-100 h-auto w-full px-12">
+    <div class="flex flex-row justify-center">
+      <ImageViewer />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
