355dc813bf43fbea46b04eadd1b936a4805e4add
fmabid <fmaabid@gmail.com>
2022-01-22 15:46:09 +0600

    created image uploader component

---
+++ src/components/Uploader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="block">
+    <div v-bind="getRootProps()">
+      <input v-bind="getInputProps()" />
+
+      <p v-if="isDragActive">Drop the files here ...</p>
+      <p v-else>Drag 'n' drop some files here, or click to select files</p>
+    </div>
+
+    <button @click="open">open</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDropzone } from "vue3-dropzone";
+
+const emit = defineEmits(["onUploaded"]);
+
+const { getRootProps, getInputProps, isDragActive, ...rest } = useDropzone({
+  onDrop,
+});
+
+function onDrop(acceptFiles, rejectReasons) {
+  emit("onUploaded", acceptFiles[0]);
+}
+</script>
