6b02f765ce84b97027e8c3f1ad511d5d7bccc739
fmabid <fmaabid@gmail.com>
2022-01-22 17:08:12 +0600

    Set common menu

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts"></script>
 
 <template>
+  <div class="text-center space-x-4 py-4">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+
   <router-view></router-view>
 </template>
 
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,6 @@
 <template>
-  <div class="text-center space-x-4 py-4">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-  </div>
-
   <div class="bg-yellow-100 h-auto w-full px-12">
-    <div class="flex flex-row justify-center">
+    <div class="flex flex-row justify-center items-center">
       <ImageViewer />
     </div>
   </div>
