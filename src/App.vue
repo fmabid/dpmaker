c45b0602e24a33cb16a9f3329fa7cc8c7522172f
Abid <fmaabid@gmail.com>
2022-08-03 21:07:46 +0600

    back

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
 
 <template>
   <div class="text-center space-x-4 py-4">
-    <!-- <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link> -->
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
   </div>
 
   <router-view></router-view>
